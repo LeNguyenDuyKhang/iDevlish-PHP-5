87da458309d8242dce3508b9ed0d5b17099fe26e
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-09-05 23:12:21 +0700

    add

--- application/logs/log-2025-09-05.php
+++ application/logs/log-2025-09-05.php
@@ -29,3 +29,17 @@ ERROR - 2025-09-05 20:54:54 --> Severity: Parsing Error --> syntax error, unexpe
 ERROR - 2025-09-05 20:55:54 --> Severity: Parsing Error --> syntax error, unexpected '?' C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\application\modules\layout\views\site\partial\post_home.php 12
 ERROR - 2025-09-05 20:57:37 --> Severity: Parsing Error --> syntax error, unexpected '?' C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\application\modules\layout\views\site\partial\post_home.php 12
 ERROR - 2025-09-05 21:03:42 --> Severity: Parsing Error --> syntax error, unexpected '?' C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\application\modules\layout\views\site\partial\post_home.php 14
+ERROR - 2025-09-05 22:21:52 --> 404 Page Not Found: 
+ERROR - 2025-09-05 22:22:17 --> 404 Page Not Found: 
+ERROR - 2025-09-05 22:34:46 --> 404 Page Not Found: 
+ERROR - 2025-09-05 22:34:48 --> 404 Page Not Found: 
+ERROR - 2025-09-05 22:47:24 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:05:55 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:06:17 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:06:21 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:06:24 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:06:27 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:06:30 --> 404 Page Not Found: 
+ERROR - 2025-09-05 23:10:17 --> Severity: Warning --> imagecopymerge() expects parameter 9 to be long, string given C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\system\libraries\Image_lib.php 1229
+ERROR - 2025-09-05 23:10:39 --> Severity: Warning --> imagecopymerge() expects parameter 9 to be long, string given C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\system\libraries\Image_lib.php 1229
+ERROR - 2025-09-05 23:10:55 --> Severity: Warning --> imagecopymerge() expects parameter 9 to be long, string given C:\TTDN_Xtech789\sample_BACK_END\Backend_PHP_5_BanHang\Xtech789_kholanh\system\libraries\Image_lib.php 1229
